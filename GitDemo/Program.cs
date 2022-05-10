b957ab6a4c81ae74defb46854801dacae6c4686c
Peyman Peirovifar <peirovifar@gmail.com>
2022-05-10 16:44:51 +0200

    Contains Error

--- GitDemo/Program.cs
+++ GitDemo/Program.cs
@@ -6,8 +6,29 @@ namespace GitDemo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("sdgjk");
+            Console.WriteLine("Enter the number of Employees:");
+            int NumUser = Int16.Parse(Console.ReadLine());
+            Console.Clear();
+            string[] name;
+            string[] salary;
+            for (int i = 0; i < NumUser; i++)
+            {
+                Console.WriteLine("Enter your name:");
+                string name[i] = Console.ReadLine();
+                Console.WriteLine("Enter your Salary:");
+                int salary[i] = Int16.Parse(Console.ReadLine());
+            }
+            Console.Clear();
+            for (int i = 0; i < NumUser; i++)
+            {
+                Console.WriteLine(name[i]);
+                
+                Console.WriteLine(salary[i]);
+                
+            }
+
+
+
         }
     }
 }
